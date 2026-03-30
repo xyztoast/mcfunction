3b5c5ad26299375fd548224fff23e561ba57aafb
toast <244019107+xyztoast@users.noreply.github.com>
2026-03-30 16:29:58 -0700

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -7,6 +7,10 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cached => {
