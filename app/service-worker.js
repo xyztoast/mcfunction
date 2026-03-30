355e423f6ad151b0142d14e645e760ca73d1185d
toast <244019107+xyztoast@users.noreply.github.com>
2026-03-30 15:53:07 -0700

    Create service-worker.js

---
+++ app/service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'mcfunction-editor-v1';
+const FILES_TO_CACHE = [
+  './',
+  './editor.html',
+  './site.css',
+  './manifest.json',
+  './app.js',
+  './icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
+  );
+});
